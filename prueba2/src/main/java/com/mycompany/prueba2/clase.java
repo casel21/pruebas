e742c23803844d25382903c02e14cee60317ef83
KEV!N <KEV!N@KEVINPC>
2023-02-28 14:17:52 +0100

    PRUEBA 3

---
+++ prueba2/src/main/java/com/mycompany/prueba2/clase.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba2;
+
+/**
+ *
+ * @author KEV!N
+ */
+public class clase 
+{
+    String nombre;
+    clase(String n)
+    {
+        this.nombre=n;
+    }
+    void verdatos()
+    {
+        System.out.println("::::::");
+        System.out.println("NOMBRE "+ this.nombre);
+    }
+}
